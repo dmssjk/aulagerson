27ca40782454de73d7db2a005cf684120f16a51c
Daniel <dmssjk@gmail.com>
2023-03-01 11:50:25 -0300

    operacoes

---
+++ index.js
@@ -0,0 +1,8 @@
+const operacoes = (valor1, valor2) => {
+  console.log(`Resultado Soma: ${valor1 + valor2}`);
+  console.log(`Resultado Subtração: ${valor1 - valor2}`);
+  console.log(`Resultado Multiplicação: ${valor1 * valor2}`);
+  console.log(`Resultado Divisão: ${valor1 / valor2}`);
+};
+
+exports.operacoesBasicas = operacoes;
